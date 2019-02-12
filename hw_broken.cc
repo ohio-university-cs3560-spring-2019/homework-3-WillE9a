f6eeac8fe3d5919ac5ec5b55139157f822c14eb8
Will Eshbaugh <we175615@ohio.edu>
2019-02-11 22:58:20 -0500

    First commit for assignment 3. Everything required should be here now.

--- hw_broken.cc
+++ hw_broken.cc
@@ -1,24 +1,33 @@
+/*! \file hw_broken.cc
+	\brief Contains the deviation function definition
+*/
+
 #include <iostream>
 #include <vector>
 #include <cmath>
 
-
-
-int deviation( int* a, int n )
+/** \fn double deviation(int* a, int n)
+	\brief Calculates the standard deviation for a set of integers using the following formula: 
+	\param a -- Array of integers
+	\param n -- Size of array
+	\return A double representing the standard deviation of array elements
+ */
+double deviation( int* a, int n )
 {
-	int sum;
-	for(size_t i = 0; i <= v.size(); i++)
+	int sum = 0;
+	for(size_t i = 0; i < n; i++)
 	{
-		sum += v[i];
+		sum += a[i];
 	} 
-	double mean = sum /= v.size();
+	double mean = sum; // changes sum to a double
+	mean = mean/n;
 	double stddev = 0;
-	for(size_t i = 0; i <= v.size() -1; i++)
+	for(size_t i = 0; i <= n-1; i++)
 	{
-		double stddev = stddev + (v[i] - mean) * (v[i] - mean); 
+		stddev = stddev + (a[i] - mean) * (a[i] - mean); 
 	}
-	stddev /= v.size();
-	if( stddev = 0)
+	stddev /= n;
+	if( stddev == 0)
 		std::cout << "Sigma is zero." << std::endl;
 	return sqrt(stddev);
 }
